b94954ee66d30ad7e65fd25872f5e4ac7f470d37
Amy Mok <amy.mok@cfpb.gov>
2016-08-04 03:02:17 -0400

    add unit tests for pipeline/main

--- tests/main_tests.py
+++ tests/main_tests.py
@@ -1,10 +1,37 @@
 import unittest
 from nose.tools import assert_equal
+from nose_parameterized import parameterized
 from elasticizer import __main__
 
+# class AddTestCase(unittest.TestCase):
+#     @parameterized.expand([
+#         ("2 and 3", 2, 3, 5),
+#         ("3 and 5", 2, 3, 5),
+#     ])
+#     def test_add(self, _, a, b, expected):
+#         assert_equal(a + b, expected)
 
-class MyTest(unittest.TestCase):
+class TestBackupType(unittest.TestCase):
 
-    def backup0_test(self):
+    @parameterized.expand([
+        ("int more than or equal to 1", 1, 1),
+        ("str more than or equal to 1", "1", 1),
+    ])
+    def test_backup(self, _, bu_type, expected):
+        assert_equal(__main__.backup_type(bu_type), expected)
+
+    @parameterized.expand([
+        ("int less than 1", 0),
+        ("str less than 1", "0"),
+    ])
+    def test_backup_with_exception(self, _, bu_type):
         with self.assertRaises(Exception):
-            __main__.backup_type(0)
+            __main__.backup_type(bu_type) 
+
+
+# # class TestClear(unittest.TestCase):
+# #     """docstring for ClassName"""
+    
+# #     def clear_test(self):
+
+        
--- tests/pipeline_tests.py
+++ tests/pipeline_tests.py
@@ -1,9 +1,10 @@
 import unittest
 from nose.tools import assert_equal
 from elasticizer.pipelines.extract_postgres import Extract
-from elasticizer import Load
+from elasticizer import Load, ValidSettings, ExternalLocalTarget, Format
 from nose_parameterized import parameterized
 import collections
+from mock import mock_open, patch
 
 class TestIndexNamer(unittest.TestCase):
 
@@ -22,4 +23,43 @@ class TestIndexNamer(unittest.TestCase):
         expected = Indexes(*expected)
         index_names = Load.label_indices(n_versions, name)
         assert_equal(index_names, expected)
+
+class TestExternalLocalTarget(unittest.TestCase):
+    def init_test(self):
+        pass
+    def remove_test(self):
+        pass
+
+class TestExtract(unittest.TestCase):
+    def query_test(self):
+        extract = Extract("table")
+        assert_equal(extract.query, 'SELECT * FROM table')
+
+class TestFormat(unittest.TestCase):
+
+    def fields_from_mapping_test(self):
+        with patch('__builtin__.open', mock_open(read_data='bibble'), create=True) as m:            
+            format = Format("mapping_file", "docs_file", "table")
+            format._fields_from_mapping()
+        m.assert_called_once_with('mapping_file')
+
+# >>> with patch('__main__.open', mock_open(read_data='bibble'), create=True) as m:
+# ...     with open('foo') as h:
+# ...         result = h.read()
+# ...
+# >>> m.assert_called_once_with('foo')
+# >>> assert result == 'bibble'
+
+class TestValidSettings(unittest.TestCase):
+
+    def requires_test(self):
+        v_settings = ValidSettings("mapping_file")
+        assert_equal(v_settings.requires(), [])
+
+    def output_test(self):
+        v_settings = ValidSettings("mapping_file")
+        self.assertIsInstance(v_settings.output(), ExternalLocalTarget)
+
+
+
 
