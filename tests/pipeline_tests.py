a16a9fe12e59d2cbda5812b4baae4339f4bc17de
Amy Mok <amy.mok@cfpb.gov>
2016-08-05 17:37:36 -0400

    Add partial format.run test

--- tests/pipeline_tests.py
+++ tests/pipeline_tests.py
@@ -1,10 +1,13 @@
 import unittest
 from nose.tools import assert_equal
-from elasticizer.pipelines.extract_postgres import Extract
-from elasticizer import Load, ValidSettings, ExternalLocalTarget, Format
+from elasticizer.pipelines.extract_postgres import Extract, ValidMapping, ValidSettings, ExternalLocalTarget, Format
+from elasticizer import Load
 from nose_parameterized import parameterized
 import collections
-from mock import mock_open, patch
+from luigi import LocalTarget
+# from luigi.test.postgre_test import MockPostgresCursor
+from mock import mock_open, patch, Mock, MagicMock, mock#, mock_calls
+from collections import namedtuple
 
 class TestIndexNamer(unittest.TestCase):
 
@@ -38,17 +41,94 @@ class TestExtract(unittest.TestCase):
 class TestFormat(unittest.TestCase):
 
     def fields_from_mapping_test(self):
-        with patch('__builtin__.open', mock_open(read_data='bibble'), create=True) as m:            
+        data ='''
+        {
+            "_all": {
+                "type": "string",
+                "analyzer": "med_search_analyzer",
+                "search_analyzer": "med_search_analyzer"
+            },
+            "properties": {
+                "id_cfpb": {
+                    "type": "string",
+                    "copy_to": "all_query"
+                },
+                "name": {
+                    "type": "string",
+                    "copy_to": "all_suggester"
+                },
+                "all_suggester": {
+                    "type": "string",
+                    "analyzer": "autocomplete",
+                    "search_analyzer": "standard"
+                },
+                "all_query": {
+                    "type": "string",
+                    "analyzer": "autocomplete",
+                    "search_analyzer": "standard"
+                }
+            }
+        }
+        '''
+        with patch('__builtin__.open', mock_open(read_data=data), create=True) as m:            
             format = Format("mapping_file", "docs_file", "table")
-            format._fields_from_mapping()
-        m.assert_called_once_with('mapping_file')
+            result = format._fields_from_mapping()
+        m.assert_called_once_with('mapping_file', 'r')
+        assert_equal([u'id_cfpb', u'name'], result)
 
-# >>> with patch('__main__.open', mock_open(read_data='bibble'), create=True) as m:
-# ...     with open('foo') as h:
-# ...         result = h.read()
-# ...
-# >>> m.assert_called_once_with('foo')
-# >>> assert result == 'bibble'
+    def projection_test(self):
+        pass
+
+    @patch('elasticizer.pipelines.extract_postgres.ValidMapping')
+    @patch('elasticizer.pipelines.extract_postgres.Extract')
+    def requires_test(self, mock_extract, mock_valid_mapping):
+        mock_extract.__class__ = Extract
+        mock_valid_mapping.__class__ = ValidMapping
+        format = Format("mapping_file", "docs_file", "table")
+        result = format.requires()
+        assert mock_extract.called
+        assert mock_valid_mapping.called
+        assert_equal(len(result), 2)
+        # self.assertIsInstance(result[0], Extract)
+        # self.assertIsInstance(result[1], ValidMapping)
+        # self.assertTrue(isinstance(result[0], Extract))
+        # @TODO: Check the result[0] is extract instance
+        # @TODO: Check the result[1] is valid mapping instance
+
+    def output_test(self):
+        format = Format("mapping_file", "docs_file", "table")
+        result = format.output()
+        self.assertIsInstance(result, LocalTarget)
+        # @TODO: Check mapping file is passed into LocalTarget
+
+    @patch.object(Format, '_projection')
+    @patch.object(Format, 'input')
+    @patch.object(Format, '_fields_from_mapping')
+    def run_test(self, mock_field_map, mock_input, mock_projection):
+        mock_field_map.return_value = [u'id_cfpb', u'name']
+
+        mock_cursor_attrs = {'execute.return_value': True}
+        mock_cursor = MagicMock(**mock_cursor_attrs)
+        mock_cursor_enter_return = MagicMock(return_value=["row1", "row2", "row3"], **mock_cursor_attrs)
+        mock_cursor_enter_return.__iter__.return_value = ['row1', 'row2', 'row3']
+        mock_cursor.__enter__ = Mock(return_value=mock_cursor_enter_return)
+        mock_connect_attrs = {'cursor.return_value': mock_cursor}
+        mock_connect = Mock(**mock_connect_attrs)
+        mock_extractor_attrs = {'connect.return_value': mock_connect}
+        mock_extractor = Mock(table="table", **mock_extractor_attrs)
+        mock_input.return_value = [mock_extractor]
+
+        mock_projection.return_value = "1"
+        format = Format("mapping_file", "docs_file", "table")
+        format.run()
+
+        mock_field_map.assert_called_once()
+        assert_equal(mock_projection.call_count, 3)
+        mock_projection.assert_has_calls([mock.call("row1", mock_field_map.return_value), 
+                                            mock.call("row2", mock_field_map.return_value),
+                                            mock.call("row3", mock_field_map.return_value)])
+
+        # @TODO: mock open and json dump
 
 class TestValidSettings(unittest.TestCase):
 
