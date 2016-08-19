3226e8e2832c4a1d46dc538b976c6bb487be2ebc
Amy Mok <amy.mok@cfpb.gov>
2016-08-18 22:51:55 -0400

    Remove commented out code

--- tests/main_tests.py
+++ tests/main_tests.py
@@ -3,13 +3,6 @@ from nose.tools import assert_equal
 from nose_parameterized import parameterized
 from elasticizer import __main__
 
-# class AddTestCase(unittest.TestCase):
-#     @parameterized.expand([
-#         ("2 and 3", 2, 3, 5),
-#         ("3 and 5", 2, 3, 5),
-#     ])
-#     def test_add(self, _, a, b, expected):
-#         assert_equal(a + b, expected)
 
 class TestBackupType(unittest.TestCase):
 
@@ -28,10 +21,5 @@ class TestBackupType(unittest.TestCase):
         with self.assertRaises(Exception):
             __main__.backup_type(bu_type) 
 
-
-# # class TestClear(unittest.TestCase):
-# #     """docstring for ClassName"""
-    
-# #     def clear_test(self):
 
         
